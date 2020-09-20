c345ad170091fd2345ac733fadd31cd76da702dd
trongki07 <trongki07@gmail.com>
2020-09-20 12:29:09 +0700

    change port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 const publicPath = path.join(__dirname, '/../public');
